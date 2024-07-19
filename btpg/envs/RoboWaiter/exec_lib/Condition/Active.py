deba97c6a381c5db2ca7204e49f7313aee6d59bd
DIDS-EI <DIDS_EI@163.com>
2024-07-19 10:31:05 +0800

    Update Active.py

--- btpg/envs/RoboWaiter/exec_lib/Condition/Active.py
+++ btpg/envs/RoboWaiter/exec_lib/Condition/Active.py
@@ -1,6 +1,6 @@
 from btpg.envs.RoboWaiter.exec_lib._base.RWCondition import RWCondition
 
-class Activate(RWCondition):
+class Active(RWCondition):
     can_be_expanded = True
     num_args = 1
     valid_args = {'AC','TubeLight','HallLight'}
